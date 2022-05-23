60f8e5c43bca4796c0bc944e1c0aeb15ee5d47f6
tamnalee <105832393+tamnalee@users.noreply.github.com>
2022-05-23 16:56:40 +0900

    add

---
+++ app.py
@@ -0,0 +1,22 @@
+from numpy import imag
+import streamlit as st
+from app_home import run_home
+from app_eda import run_eda
+from app_ml import run_ml
+from PIL import Image
+
+def main():
+    st.title('자동차 가격 예측 App')
+
+    menu = ['Home','EDA','ML']
+    choice = st.sidebar.selectbox('메뉴선택',menu)
+    if choice == menu[0]:
+        run_home()
+    elif choice == menu[1]:
+        run_eda()
+    elif choice == menu[2]:
+        run_ml()
+
+
+if __name__ == '__main__':
+    main()
