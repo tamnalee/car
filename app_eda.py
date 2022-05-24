2a871415e16e156bed90550c891f75ca9fd38be9
tamnalee <105832393+tamnalee@users.noreply.github.com>
2022-05-24 10:06:30 +0900

    add

--- app_eda.py
+++ app_eda.py
@@ -8,6 +8,8 @@ def run_eda():
     st.subheader('데이터 분석')
     df = pd.read_csv('data/Car_Purchasing_Data.csv', encoding='ISO-8859-1')
 
+    st.text('기본적인 데이터 분석입니다.')
+
     # 라디오 버튼을 이용하여 데이터프레임과 통계치를 선택래서 볼수있게 한다.
 
     radio_menu = ['데이터프레임','통계치']
